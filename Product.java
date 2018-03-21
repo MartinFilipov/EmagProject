0b50a16f8e6c96ef7a8b9be1d44183c42594b2cc
Radoslav Rashev <radoslav.rashev@abv.bg>
2018-03-21 17:01:13 +0200

    Added Product.java

---
+++ Product.java
@@ -0,0 +1,59 @@
+
+public abstract class Product {
+	
+	private String name;
+	private double price;
+	private int quantity;
+	private Warranty warranty;
+	
+	public Product(String name, double price, int quantity) {
+		setName(name);
+		setPrice(price);
+		setQuantity(quantity);
+	}
+	
+	//----------------------------------------METHODS----------------------------------------
+	
+	public abstract int getWarrantyPeriodInMonths();
+	
+	//----------------------------------------SETTERS----------------------------------------
+
+	protected void setName(String name) {
+		if (name != null && name.trim().length() > 0) {
+			this.name = name;
+		} else {
+			this.name = "Unknown";
+		}
+	}
+
+	protected void setPrice(double price) {
+		this.price = price > 0 ? price : 0;
+	}
+
+	protected void setQuantity(int quantity) {
+		this.quantity = quantity > 0 ? quantity : 0;
+	}
+
+	protected void setWarranty(Warranty warranty) {
+		if (warranty != null) {
+			this.warranty = warranty;
+		} else {
+			this.warranty = null;
+		}
+	}
+
+	//----------------------------------------GETTERS----------------------------------------
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	
+}
