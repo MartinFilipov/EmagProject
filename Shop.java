2268dee7d1ab1abb0eada68c94ecdd1aeeb82f85
Martin Filipov <martin.i.filipov@gmail.com>
2018-03-26 16:56:54 +0300

    added functionality to the Shop class (chooseSupplierOfProduct / sell and addToCart

--- Shop.java
+++ Shop.java
@@ -1,6 +1,9 @@
+package Classes;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Shop {
@@ -89,5 +92,88 @@ public class Shop {
 			this.money += money;
 		}
 	}
+	
+	// Chooses a supplier for the selected product.
+	// Returns the productID of the selected product.
+	int chooseSupplierOfProduct(String productName) throws InvalidProductNameException{
+		if(productName==null || !products.containsKey(productName)){
+			System.out.println("");
+			throw new InvalidProductNameException("Invalid product name!");
+		}
+		Scanner in=new Scanner(System.in);
+		int index=1;
+		for(Product p:products.get(productName).values()){
+			System.out.println("Index: "+(index++)+" "+ p);
+		}
+		int insertIndex=0;
+		do{
+			System.out.print("Pick a product index: ");
+			insertIndex=in.nextInt();
+		}while(insertIndex<1 || insertIndex>products.get(productName).values().size());
+		index=1;
+		int productID=0;
+		for(Product p:products.get(productName).values()){
+			if(index++==insertIndex){
+				productID= p.getProductId();
+			}
+		}
+		return productID;
+	}
+	
+	void addToCart(Cart cart, String productName,int id){
+		cart.addProduct(products.get(productName).get(id));
+	}
+	
+	private boolean sell(Product product, User user, int quantity) {
+		if (product != null) {
+			if (decreaseQuantityOfProductInShop(product,quantity)) {
+				increaseMoney(product.getPrice() * quantity*10/100);
+			}else{ 
+				System.out.println("Something went wrong.");
+				return false;
+			}
+			user.removeMoneyForProduct(product.getPrice()*quantity);
+			user.addProduct(product.clone(quantity));
+			paySupplier(this.suppliers.get(product.getProductId()),product.getPrice()*quantity*90/100);
+			return true;
+		}
+		return false;
+	}
+	
+	//Sells the list of products to the user, depending on the quantities he/she set
+	public boolean sell(List<Product> products, User user, int[] quantities) {
+		if(products!=null && user!=null){
+			boolean result=true;
+			int indexOfQuantities=0;
+			for(Product p: products){
+				result = result && this.sell(p, user, quantities[indexOfQuantities++]);
+			}
+			return result;
+		}
+		return false;
+	}
+	private boolean sell(Product product, User user, int quantity, Voucher voucher){
+		
+		return false;
+	}
+	public boolean sell(List<Product> products2, User user, int[] quantities, Voucher voucher) {
+				
+		return false;
+	}
+	
+	private boolean decreaseQuantityOfProductInShop(Product product,int quantity) {
+		if (product != null) {
 
+			if (this.products.get(product.getName()).get(product.getProductId()).getQuantity() >= quantity) {
+				this.products.get(product.getName()).get(product.getProductId()).reduceQuantity(quantity);
+				return true;
+			} else {
+				System.out.println("Not enough of this product in the shop");
+			}
+		}
+		return false;
+	}
+
+
+	
 }
